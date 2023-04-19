83bce41561943bf288e85595c304ffd7db1ef1c7
bengo3022 <randommanboiman@gmail.com>
2023-04-19 17:51:59 -0400

    Modified Projects Page

--- projects/index.php
+++ projects/index.php
@@ -1,5 +1,5 @@
 <head>
-    <h1 class="page-title">Under Development</h1>
+    <h1 class="page-title">My Projects</h1>
     <link rel="stylesheet" type="text/css" href="/static/css/nav_style.css">
     <link rel="stylesheet" type="text/css" href="/static/css/footer_style.css">
     <link rel="stylesheet" type="text/css" href="/static/css/project_style.css">
@@ -8,7 +8,20 @@
     <?php
 		require __DIR__ . '../../navbar.php';
 	?>
-    <img class="project-card-image" src="../static/images/not_done.jpg">
+    <div class="header-container">
+		<div class="page-background"></div>
+		<div class="page-container">
+			<h1 class="page-title" id="page-title">PROJECTS</h1>
+			<p class="page-description">
+                Here you will find a collection of my past and current projects.
+                Each project has been a unique learning experience. From software
+                development to web design, these projects showcase my skills
+                and interests. Feel free to explore and delve deeper into my 
+                professional experience.
+			</p>
+		</div>
+	</div>
+    <img class="project-card-image" src="/static/images/not_done.jpg">
     <?php
         require __DIR__ . '../../footer.php';
 	?>
