fa4d83329029108b68583c1d210fb18c5d0589fa
bengo3022 <randommanboiman@gmail.com>
2023-04-29 17:58:42 -0400

    Modified and Rerouted error page

--- error404/index.php
+++ error404/index.php
@@ -1,20 +1,33 @@
 <head>
+  <!-- Styles -->
   <link rel="stylesheet" type="text/css" href="/static/css/nav_style.css">
   <link rel="stylesheet" type="text/css" href="/static/css/footer_style.css">
   <link rel="stylesheet" type="text/css" href="/static/css/error_style.css">
 </head>
 
 <body>
+  <!-- NAVBAR -->
   <?php
-		require __DIR__ . '../navbar.php';
+		require __DIR__ . '../../navbar.php';
 	?>
+  <!-- NAVBAR END -->
+
+  <!-- ERROR -->
   <div class = "Error">
     <h1 id = "Error-Header">404</h1>
     <h2 id = "Error-SubHeader">Page not found</h2>
     <p id = "Error-paragraph">The page you are looking for does not exist.</p>
     <p><a href="/">Visit Homepage</a></p>
   </div>
+  <!-- ERROR END -->
+
+  <!-- FOOTER -->
   <?php
-		require __DIR__ . '../footer.php';
+		require __DIR__ . '../../footer.php';
 	?>
+  <!-- FOOTER END -->
+
+  <!-- SCRIPTS-->
+	<script src="/static/javascript/navbar_transition.js"></script>
+	<!-- SCRIPTS END -->
 </body>
