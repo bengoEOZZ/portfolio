fffaa6f9c2d63aeadfbe1a835543c935a277cff1
bengoEOZZ <121465379+bengoEOZZ@users.noreply.github.com>
2025-03-22 17:32:35 -0400

    Home Page: Added entire Draggable Background

--- static/javascript/drag.js
+++ static/javascript/drag.js
@@ -1,4 +1,4 @@
-// Make the DIV element draggable:
+// Make the Screen DIV element draggable:
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id)) {
