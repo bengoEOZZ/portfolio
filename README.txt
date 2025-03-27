9f01e3133a729cf1eb1b05ea42bae535b26722b2
bengoEOZZ <121465379+bengoEOZZ@users.noreply.github.com>
2025-03-26 23:24:18 -0400

    README: Drafted upcoming changes

--- README.txt
+++ README.txt
@@ -1,1 +1,4 @@
-Nothing for now
+- Add sun: Alternating rays
+- Add daylight/night-time changes
+- Add seasonal changes
+- Add adjustable time clock
--- indexNew.php
+++ indexNew.php
@@ -6,6 +6,7 @@
     <script src="/static/javascript/circle-navbar.js"></script>
     <script src="/static/javascript/circle-animation.js"></script>
     <link rel="stylesheet" type="text/css" href="/static/css/index_style.css">
+    <link rel="stylesheet" type="text/css" href="/static/css/navbar_new.css">
 </head>
 <body>
     
@@ -13,9 +14,10 @@
         <img src="/static/images/Ben.svg" alt="Benjamin Tiong SVG" class="planet" id="draggable"></img>
         <div class="left-content">
             <h1 class="left-text">Hello World...</h1>
-            <p class="intro">I'm <strong>Benjamin Tiong</strong>, a software developer where my creativity orbits around code and innovation. Explore my universe and get to know the developer behind the pixels!</p>
+            <p class="intro">I'm <strong>Benjamin Tiong</strong>, a passionate software developer whose creativity orbits around the endless possibilities of code and innovation. Dive into my universe, where I bring my most and explore the stories behind the pixels that bring my projects to life. Let's embark on a journey of discovery and inspiration together!</p>
         </div>
 
+        <img src="/static/images/earth.svg" alt="Earth" class="earth-svg">
         <!--<div id="mydiv">
             <div id="mydivheader">Click here to move</div>
             <p>Move</p>
@@ -49,15 +51,15 @@
                 <span class="btn-text">Contact</span>
             </a>
         </div>
-        <!-- Placeholder for the navigation bar 
-        <nav class="navbar hidden">
+
+        <nav class="navbar">
             <ul>
                 <li><a href="#about">About</a></li>
                 <li><a href="#coding">Coding</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
-        </nav>-->
+        </nav>
     </div>
     <!--<script src="/static/javascript/drag.js"></script>-->
 </body>
