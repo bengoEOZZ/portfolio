e64bfb550cbd5c680b0474d67094838ca71dc283
bengoEOZZ <121465379+bengoEOZZ@users.noreply.github.com>
2025-04-06 20:46:45 -0400

    Home Page (Creative): Added Fall MapleLeaves animation to seasonalChanges
    - Add varied maple leaf sizes, colours, and positions
    - Implement natural swaying and rotation animation for horizontal movement
    - Created maple leaf shape using CSS clip-path

--- src/components/CelestialObjects/index.jsx
+++ src/components/CelestialObjects/index.jsx
@@ -3,23 +3,32 @@ import classes from './CelestialObjects.module.css';
 
 import BenImage from '../../assets/Ben.svg';
 import EarthImage from '../../assets/earth2.svg';
+import EarthFallImage from '../../assets/earthFall.svg';
 import SunImage from '../../assets/sun.svg';
 
 function CelestialObjects() {
     const snowflakes = Array.from({ length: 25 })
+    const mapleleaves = Array.from({ length: 25 })
 
     return (
         <>
             {/*<img src={BenImage} alt="Benjamin Tiong SVG" className={classes.planet} />*/}
             <div className={classes.seasonalChanges}>
+                {/*}
                 <div className={classes.snowflakes}>
                     {snowflakes.map((_, index) => (
                         <i key={index} className={classes.snowflake}></i>
+                    ))}
+                </div>*/}
+                <div className={classes.mapleLeaves}>
+                    {mapleleaves.map((_, index) => (
+                        <i key={index} className={classes.mapleLeaves}></i>
                     ))}
                 </div>
             </div>
 
             <img src={EarthImage} alt="Earth" className={classes.earth} />
+            <img src={EarthFallImage} alt="Earth" className={classes.earthSeason}/>
             {/*<img src={SunImage} alt="Sun" className={classes.sun} />
             <div className={classes.sunrays}>
                 <SunRays />
