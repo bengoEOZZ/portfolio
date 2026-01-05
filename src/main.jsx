99ff925890a4fcdcf58f90292b8b1e417122c6e3
bengoEOZZ <121465379+bengoEOZZ@users.noreply.github.com>
2026-01-05 04:05:56 -0500

    Mobile: Reversed Previous Commit Change + Added simple scroll

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Scroll down slightly after a delay to hide address bar on mobile
+if (window.matchMedia('(max-height: 600px) and (orientation: landscape)').matches) {
+  setTimeout(() => {
+    window.scrollTo(0, 1);
+  }, 2000);
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
