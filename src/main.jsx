3f30ae3b0468d896474c02ec278435ce9a2f2589
bengoEOZZ <121465379+bengoEOZZ@users.noreply.github.com>
2026-01-05 04:10:23 -0500

    Mobile: Adjusted responsive sizing changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 // Scroll down slightly after a delay to hide address bar on mobile
-if (window.matchMedia('(max-height: 600px) and (orientation: landscape)').matches) {
+if (window.matchMedia('(max-width: 600px)').matches) {
   setTimeout(() => {
     window.scrollTo(0, 1);
   }, 2000);
